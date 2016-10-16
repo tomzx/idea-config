1ff2305bea2fa91c4cd117c7a5cc2bcec62b362d
Tom Rochette <roctom@gmail.com>
2016-10-16 19:54:57 -0400

    Add color and inspection configuration support. Refactor setup.bat and setup.sh.

--- setup.bat
+++ setup.bat
@@ -1,14 +1,26 @@
 setlocal enabledelayedexpansion
 
+goto :main
+
+:createSymlink
+	mklink %TARGET%\codestyles\eXolnet.xml %BASE%\codestyles\eXolnet.xml
+	mklink %TARGET%\colors\Tom.icls %BASE%\colors\Tom.icls
+	mklink %TARGET%\inspection\eXolnet.xml %BASE%\inspection\eXolnet.xml
+	mklink %TARGET%\templates\eXolnet.xml %BASE%\templates\eXolnet.xml
+goto :eof
+
+:main
 set BASE=%cd%
 for %%i in (7 8 9 10) do (
 	set TARGET=%USERPROFILE%\.WebIde%%i0\config
-	mklink !TARGET!\codestyles\eXolnet.xml %BASE%\codestyles\eXolnet.xml
-	mklink !TARGET!\templates\eXolnet.xml %BASE%\templates\eXolnet.xml
+	IF EXIST !TARGET! (
+		call :createSymlink
+	)
 )
 
 for %%s in (2016.1 2016.2 2016.3) do (
 	set TARGET=%USERPROFILE%\.PhpStorm%%s\config
-	mklink !TARGET!\codestyles\eXolnet.xml %BASE%\codestyles\eXolnet.xml
-	mklink !TARGET!\templates\eXolnet.xml %BASE%\templates\eXolnet.xml
+	IF EXIST !TARGET! (
+		call :createSymlink
+	)
 )
--- setup.sh
+++ setup.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -u
 
+createSymlink() {
+	ln -s $BASE/codestyles/eXolnet.xml $TARGET/codestyles/eXolnet.xml
+	ln -s $BASE/colors/Tom.icls $TARGET/colors/Tom.icls
+	ln -s $BASE/inspection/eXolnet.xml $TARGET/inspection/eXolnet.xml
+	ln -s $BASE/templates/eXolnet.xml $TARGET/templates/eXolnet.xml
+}
+
 BASE=`pwd`
 for i in 7 8 9 10
 do
 	TARGET=$HOME/Library/Preferences/WebIde${i}0
-	mkdir -p $TARGET/codestyles $TARGET/templates
-	ln -s $BASE/codestyles/eXolnet.xml $TARGET/codestyles/eXolnet.xml
-	ln -s $BASE/templates/eXolnet.xml $TARGET/templates/eXolnet.xml
+	createSymlink
 done
 
 for i in 2016.1 2016.2 2016.3
 do
 	TARGET=$HOME/Library/Preferences/PhpStorm${i}
-	mkdir -p $TARGET/codestyles $TARGET/templates
-	ln -s $BASE/codestyles/eXolnet.xml $TARGET/codestyles/eXolnet.xml
-	ln -s $BASE/templates/eXolnet.xml $TARGET/templates/eXolnet.xml
+	createSymlink
 done
+
